69e78e1912e29571374816505ae54e0aa7449306
PratikPttk <pratkparhd@gmail.com>
2023-03-04 01:07:37 +0530

    sadas

--- ok/src/main/java/com/ok/MainClass.java
+++ ok/src/main/java/com/ok/MainClass.java
@@ -4,13 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		
 		System.out.println("Hello Japan");
 		System.out.println("Hello is the world");
-=======
-		System.out.println("Hello Space");
->>>>>>> branch 'main' of https://github.com/PratikPttk/cdac-files.git
 
 	}
 
